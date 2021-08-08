4315d4a778c33dc2f81abcf837769a3598cba663
yuoku <appledev.yuoku@gmail.com>
2021-08-08 13:31:36 +0900

    Initial Commit

---
+++ SwiftUICustomTransition/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICustomTransition
+//
+//  Created by Yuki Okudera on 2021/08/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
